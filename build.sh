6430fdc99f186e9efee0dab8c89f97e196544f45
Javi Munoz <javimudi@users.noreply.github.com>
2019-04-22 01:31:37 +0200

    Path

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 docker build . -t 12tech/imagemagick:amazonlinux-latest
-docker run -it --rm -v ${PWD}/imagemagick:/tmp 12tech/imagemagick:amazonlinux-latest "cp /opt/imagemagick /tmp"
+docker run -it --rm -v ${PWD}/imagemagick:/tmp 12tech/imagemagick:amazonlinux-latest "cp /opt/imagemagick.tgz /tmp"
